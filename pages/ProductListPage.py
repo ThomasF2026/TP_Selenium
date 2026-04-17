c597118b53f3596778df9d25648d33aeda5695f1
Thomas <thomas.fontaine2026@campus-eni.fr>
2026-04-17 16:11:16 +0200

    hahaha

--- pages/ProductListPage.py
+++ pages/ProductListPage.py
@@ -5,7 +5,7 @@ class ProductListPage():
  
     def __init__(self, driver: WebDriver):
          self.driver = driver
-         self.lbl_availablityFilter = '//label[contains(text(),"In Stock")][@for="mz-fss-0--1"]/.'
+         self.lbl_availablityFilter = '(//*[contains(text(), "In stock")])[2]/..'
          self.productItem = "(//*[@class= 'carousel-item active']/*[@class= 'lazy-load'])[3]"
  
     def clickOn_FilterOnStock(self):
